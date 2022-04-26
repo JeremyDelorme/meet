e40ee8fd98ee43f36d8c900cea257e69301d5849
JeremyDelorme <jeremydelorme23.7@gmail.com>
2022-04-26 21:04:14 +0200

    Updates

---
+++ precache-manifest.d2c42a34fbb5c70cf07f92f0ef6398ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "041e4984c5945a0cba59",
+    "url": "/meet/static/css/main.8d04e071.chunk.css"
+  },
+  {
+    "revision": "041e4984c5945a0cba59",
+    "url": "/meet/static/js/main.041e4984.chunk.js"
+  },
+  {
+    "revision": "d5e422ff41d8cd74c55d",
+    "url": "/meet/static/js/1.d5e422ff.chunk.js"
+  },
+  {
+    "revision": "bdfd2effac96dd21fe9f",
+    "url": "/meet/static/js/2.bdfd2eff.chunk.js"
+  },
+  {
+    "revision": "5cc53c181b5dfa050867",
+    "url": "/meet/static/js/runtime~main.5cc53c18.js"
+  },
+  {
+    "revision": "7e137d2a6d1234322fe1be88452c3118",
+    "url": "/meet/index.html"
+  }
+];
